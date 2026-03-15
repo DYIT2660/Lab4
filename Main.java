c3856819429d5ace8883e400e1b85ab9a886451d
Daniel Yoo <daniel.yoo0523@gmail.com>
2026-03-15 19:22:37 -0400

    completed assignment

--- Main.java
+++ Main.java
@@ -10,26 +10,50 @@ public class Main {
       Lab4.outputList(integerList);
 
       System.out.println("\n\nBubble sort results ----------------------------------------------");
+      long bubbleStartTime = System.nanoTime();
       ArrayList<Integer> bubbleSortedList = Lab4.bubbleSort(integerList);
+      long bubbleEndTime = System.nanoTime();
       Lab4.outputList(bubbleSortedList);
+      System.out.println("\nBubble sort time: " + (bubbleEndTime - bubbleStartTime) / 1000000.0 + " ms");
 
       System.out.println("\n\nInsertion sort results -------------------------------------------");
-      ArrayList<Integer> insertionSortedList = Lab4.insertionSort(integerList);  
+      long insertionStartTime = System.nanoTime();
+      ArrayList<Integer> insertionSortedList = Lab4.insertionSort(integerList);
+      long insertionEndTime = System.nanoTime();
       Lab4.outputList(insertionSortedList);
+      System.out.println("\nInsertion sort time: " + (insertionEndTime - insertionStartTime) / 1000000.0 + " ms");
     }
 }
 
 class Lab4 {
   public static ArrayList<Integer> insertionSort(ArrayList<Integer> integerList) {
     // Step 1 - Implement insertion sort algorithm here
-
-    return integerList;
+    ArrayList<Integer> list = new ArrayList<>(integerList);
+    for (int i = 1; i < list.size(); i++) {
+      int iKey = list.get(i);
+      int j = i - 1;
+      while (j >= 0 && list.get(j) > iKey) {
+        list.set(j + 1, list.get(j));
+        j--;
+      }
+      list.set(j + 1, iKey);
+    }
+    return list;
   }
 
   public static ArrayList<Integer> bubbleSort(ArrayList<Integer> integerList) {
     // Step 2 - Implement the bubble sort algorithm here
-
-    return integerList;
+    ArrayList<Integer> list = new ArrayList<>(integerList);
+    for (int i = 0; i < list.size() - 1; i++) {
+      for (int j = 0; j < list.size() - 1 - i; j++) {
+        if (list.get(j) > list.get(j + 1)) {
+          int temp = list.get(j);
+          list.set(j, list.get(j + 1));
+          list.set(j + 1, temp);
+        }
+      }
+    }
+    return list;
   }
 
   public static ArrayList<Integer> getList() {
